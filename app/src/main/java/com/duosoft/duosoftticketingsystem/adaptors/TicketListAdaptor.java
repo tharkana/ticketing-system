7b8c87dcf2d15d34885df37ca5bc0c2a5857b04c
Tharkana Kodagoda <tharkana.infor@gmail.com>
2017-06-26 16:36:18 +0530

    Revamp the UI

--- app/src/main/java/com/duosoft/duosoftticketingsystem/adaptors/TicketListAdaptor.java
+++ app/src/main/java/com/duosoft/duosoftticketingsystem/adaptors/TicketListAdaptor.java
@@ -1,20 +1,19 @@
 package com.duosoft.duosoftticketingsystem.adaptors;
 
 import android.content.Context;
+import android.os.Build;
+import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.duosoft.duosoftticketingsystem.R;
 import com.duosoft.duosoftticketingsystem.rest_api.pojo.TicketListResponse;
 
 import java.util.Collections;
 import java.util.List;
-
-import butterknife.ButterKnife;
 
 /**
  * Created by Tharkana on 6/25/2017.
@@ -49,10 +48,44 @@ public class TicketListAdaptor extends RecyclerView.Adapter<TicketListAdaptor.Ti
     public void onBindViewHolder(TicketListAdaptor.TicketViewHolder holder, int position) {
         TicketListResponse.Ticket ticket = data.get(position);
         Log.d("TK", ticket.toString() );
-        holder.textViewPriority.setText("Priority: " + ticket.getPriority());
-        holder.textViewStatus.setText("Status: " + ticket.getStatus());
-        holder.textViewSubject.setText("Subject: " + ticket.getSubject());
-        holder.textViewType.setText("Type: " + ticket.getType());
+        holder.textViewPriority.setText(ticket.getPriority());
+        holder.textViewStatus.setText( ticket.getStatus());
+        holder.textViewSubject.setText( ticket.getSubject());
+        holder.textViewType.setText( ticket.getType());
+
+        switch (ticket.getPriority()){
+            case "urgent": {
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    holder.viewSeperator.setBackgroundColor(context.getResources().getColor(R.color.priorityUrgent, null));
+                } else{
+                    holder.viewSeperator.setBackgroundColor(context.getResources().getColor(R.color.priorityUrgent));
+                }
+                break;
+            }
+            case "high": {
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    holder.viewSeperator.setBackgroundColor(context.getResources().getColor(R.color.priorityHigh, null));
+                } else{
+                    holder.viewSeperator.setBackgroundColor(context.getResources().getColor(R.color.priorityHigh));
+                }
+                break;
+            }
+            case "normal": {
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    holder.viewSeperator.setBackgroundColor(context.getResources().getColor(R.color.priorityNormal, null));
+                } else{
+                    holder.viewSeperator.setBackgroundColor(context.getResources().getColor(R.color.priorityNormal));
+                }
+                break;
+            }
+            default: {
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    holder.viewSeperator.setBackgroundColor(context.getResources().getColor(R.color.priorityNormal, null));
+                } else{
+                    holder.viewSeperator.setBackgroundColor(context.getResources().getColor(R.color.priorityNormal));
+                }
+            }
+        }
     }
 
     @Override
@@ -67,18 +100,20 @@ public class TicketListAdaptor extends RecyclerView.Adapter<TicketListAdaptor.Ti
 //        @Bind(R.id.textViewSubject)  TextView textViewSubject;
 //        @Bind(R.id.textViewType)  TextView textViewType;
 
-        TextView textViewPriority;
-        TextView textViewStatus;
-        TextView textViewSubject;
-        TextView textViewType;
+        AppCompatTextView textViewPriority;
+        AppCompatTextView textViewStatus;
+        AppCompatTextView textViewSubject;
+        AppCompatTextView textViewType;
+        View viewSeperator;
 
         public TicketViewHolder(View itemView) {
             super(itemView);
 //            ButterKnife.bind(itemView);
-            textViewPriority = (TextView) itemView.findViewById(R.id.textViewPriority);
-            textViewStatus = (TextView) itemView.findViewById(R.id.textViewStatus);
-            textViewSubject = (TextView) itemView.findViewById(R.id.textViewSubject);
-            textViewType = (TextView) itemView.findViewById(R.id.textViewType);
+            textViewPriority = (AppCompatTextView) itemView.findViewById(R.id.textViewPriority);
+            textViewStatus = (AppCompatTextView) itemView.findViewById(R.id.textViewStatus);
+            textViewSubject = (AppCompatTextView) itemView.findViewById(R.id.textViewSubject);
+            textViewType = (AppCompatTextView) itemView.findViewById(R.id.textViewType);
+            viewSeperator = (View) itemView.findViewById(R.id.priorityStrip);
         }
     }
 
